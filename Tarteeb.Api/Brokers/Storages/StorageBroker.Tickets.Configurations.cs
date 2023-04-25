233c7e70a81ebf0e0d01a5c2357a50d331507085
MirzayevDeveloper <MirzayevDeveloper1@gmail.com>
2023-04-25 16:55:43 +0500

    BROKERS: Tickets Configurations

---
+++ Tarteeb.Api/Brokers/Storages/StorageBroker.Tickets.Configurations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tarteeb.Api.Models.Foundations.Tickets;
+
+namespace Tarteeb.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public void ConfigureTicketDeleteBehavior(EntityTypeBuilder<Ticket> builder)
+        {
+            builder.Property(ticket => ticket.MilestoneId)
+                .IsRequired(false);
+
+            builder.Property(user => user.MilestoneId)
+            .IsRequired(false);
+
+            builder.HasOne(t => t.Assignee)
+                .WithMany(u => u.Tickets)
+                .HasForeignKey(t => t.AssigneeId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasOne(t => t.Milestone)
+                .WithMany(u => u.Tickets)
+                .HasForeignKey(t => t.MilestoneId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
